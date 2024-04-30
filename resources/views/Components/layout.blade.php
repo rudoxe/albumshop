8469b2968cc50581018a4582d3beadc8aeb12252
rudoxe <ipa22.r.vikmanis@vtdt.edu.lv>
2024-04-30 20:13:58 +0300

    first commit

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ya</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body>
 <!--
   This example requires updating your template:
 
@@ -52,7 +52,7 @@
                 <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                  <img class="h-8 w-8 rounded-full" src="https://i.scdn.co/image/ab6761610000517473d4facbd619ae025b5588c7" alt="">
                 </button>
               </div>
             </div>
